d521ea00d681c6df83558bd0b3b5ee53483301d0
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-10 17:03:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  attr_accessor= :name, :roster 
+  
+  def initialize (name)
+   @name= name 
+   @roster= {} 
+   end
+   
+   def roster 
+     @roster 
+   end 
+   
+   def add_student (student, grade)
+     if @roster[grade] != nil  
+     @roster[grade] << student
+   else
+     @roster[grade]  = [student]
+   end 
+   end 
+   
+   def grade (grade)
+     @roster[grade]
+   end 
+   
+   def sort 
+     @roster.each do |student, grade|
+       grade.sort!
+     end 
+   end 
+     
+end 
